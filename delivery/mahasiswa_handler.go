5c24f93e8514290b8e7559b620deea6dcc9066cf
Muhammad Aliansyah <aliansyaahian@gmail.com>
2020-10-17 19:19:03 +0700

    Handle request tipe data DB sql.NullInt32 agar tidak null ketika insert

--- delivery/mahasiswa_handler.go
+++ delivery/mahasiswa_handler.go
@@ -13,6 +13,8 @@ import (
 	// "strings"
 	// "text/template"
 	// "github.com/julienschmidt/httprouter"
+	"fmt"
+	"database/sql"
 )
 
 // ResponseError represent the response error struct
@@ -78,20 +80,106 @@ func isRequestValid(m *domain.Mahasiswa) (bool, error) {
 	return true, nil
 }
 
+func validateInput(c echo.Context) (sql.NullInt32, error) {
+	var mahasiswa domain.Mahasiswa
+	var sem sql.NullInt32
+
+	reqSem := c.FormValue("semester")
+	// fmt.Println(reqSem)
+	if reqSem != "" {
+		fmt.Println("semester not empty")
+		if err := sem.Scan(reqSem); err != nil {
+			// panic(err)
+			// return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+			return sem, err
+		}
+	}
+	mahasiswa.Semester = sem
+
+	// fmt.Println(&mahasiswa)
+	// fmt.Println(mahasiswa.Semester)
+	// fmt.Printf("var semester = %T\n", semester)
+	// fmt.Printf("var mahasiswa.Semester = %T\n", mahasiswa.Semester)
+
+	// return true, nil
+	return mahasiswa.Semester, nil
+}
+
+func validateInputNull(c echo.Context, m domain.Mahasiswa) (domain.Mahasiswa, error) {
+	// var mahasiswa domain.Mahasiswa
+	var sem sql.NullInt32
+
+	reqSem := c.FormValue("semester")
+	// fmt.Println(reqSem)
+	if reqSem != "" {
+		fmt.Println("semester not empty")
+		if err := sem.Scan(reqSem); err != nil {
+			// panic(err)
+			// return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+			return m, err
+		}
+	}
+	// mahasiswa.Semester = sem
+	m.Semester = sem
+
+	// fmt.Println(&mahasiswa)
+	// fmt.Println(mahasiswa.Semester)
+	// fmt.Printf("var semester = %T\n", semester)
+	// fmt.Printf("var mahasiswa.Semester = %T\n", mahasiswa.Semester)
+
+	// return true, nil
+	return m, nil
+}
+
 func (m *MahasiswaHandler) Store(c echo.Context) (err error) {
 	var mahasiswa domain.Mahasiswa
+
 	err = c.Bind(&mahasiswa)
 	if err != nil {
 		return c.JSON(http.StatusUnprocessableEntity, err.Error())
 	}
+	fmt.Println(&mahasiswa)
+	fmt.Println(mahasiswa.Semester)
 
 	var ok bool
 	if ok, err = isRequestValid(&mahasiswa); !ok {
 		return c.JSON(http.StatusBadRequest, err.Error())
 	}
+	
+	/* Handle request tipe data DB sql.NullInt32 agar tidak null ketika insert */
+	// var semester sql.NullInt32
+	// sem := c.FormValue("semester")
+	// fmt.Println(sem)
+	// if sem != "" {
+	// 	fmt.Println("semester not empty")
+	// 	if err := semester.Scan(sem); err != nil {
+	// 		// panic(err)
+	// 		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
+	// 	}
+	// }
+	// mahasiswa.Semester = semester
+	// fmt.Println(&mahasiswa)
+	// fmt.Println(mahasiswa.Semester)
+
+	/* Handle request tipe data DB sql.NullInt32 agar tidak null ketika insert */
+	// var sem sql.NullInt32
+	// if sem, err = validateInput(c); err != nil {
+	// 	return c.JSON(http.StatusBadRequest, err.Error())
+	// }
+	// mahasiswa.Semester = sem
+	// fmt.Println(&mahasiswa)
+	// fmt.Println(mahasiswa.Semester)
+
+	if mahasiswa, err = validateInputNull(c, mahasiswa); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	fmt.Println(&mahasiswa)
+	fmt.Println(mahasiswa)
 
 	ctx := c.Request().Context()
 	err = m.MUsecase.Store(ctx, &mahasiswa)
+	fmt.Println(&mahasiswa)
+
 	if err != nil {
 		return c.JSON(getStatusCode(err), ResponseError{Message: err.Error()})
 	}
