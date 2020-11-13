a36c5318de1330611e8302aadc3852fad330d95f
aliansyah <aliansyaahian@gmail.com>
2020-11-13 16:49:02 +0700

    dump structure & data dbcampus

---
+++ campus.sql
@@ -0,0 +1,129 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : MySQL Local
+ Source Server Type    : MySQL
+ Source Server Version : 100119
+ Source Host           : localhost:3306
+ Source Schema         : campus
+
+ Target Server Type    : MySQL
+ Target Server Version : 100119
+ File Encoding         : 65001
+
+ Date: 13/11/2020 16:48:02
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dosen
+-- ----------------------------
+DROP TABLE IF EXISTS `dosen`;
+CREATE TABLE `dosen`  (
+  `id_dosen` int(255) NOT NULL AUTO_INCREMENT,
+  `nip` int(10) NULL DEFAULT NULL,
+  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_dosen`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for kelas
+-- ----------------------------
+DROP TABLE IF EXISTS `kelas`;
+CREATE TABLE `kelas`  (
+  `id_kelas` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `ruang_id` int(255) NULL DEFAULT NULL,
+  `mata_kuliah_id` int(255) NULL DEFAULT NULL,
+  `dosen_id` int(255) NULL DEFAULT NULL,
+  `mahasiswa_id` int(255) NULL DEFAULT NULL,
+  `created_by` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_by` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_kelas`) USING BTREE,
+  INDEX `fk_ruang`(`ruang_id`) USING BTREE,
+  INDEX `fk_mata_kuliah`(`mata_kuliah_id`) USING BTREE,
+  INDEX `fk_dosen`(`dosen_id`) USING BTREE,
+  INDEX `fk_mahasiswa`(`mahasiswa_id`) USING BTREE,
+  CONSTRAINT `fk_ruang` FOREIGN KEY (`ruang_id`) REFERENCES `ruang` (`id_ruang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dosen` FOREIGN KEY (`dosen_id`) REFERENCES `dosen` (`id_dosen`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_mahasiswa` FOREIGN KEY (`mahasiswa_id`) REFERENCES `mahasiswa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_mata_kuliah` FOREIGN KEY (`mata_kuliah_id`) REFERENCES `mata_kuliah` (`id_mata_kuliah`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for mahasiswa
+-- ----------------------------
+DROP TABLE IF EXISTS `mahasiswa`;
+CREATE TABLE `mahasiswa`  (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nim` int(10) NULL DEFAULT NULL,
+  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `semester` int(2) NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of mahasiswa
+-- ----------------------------
+INSERT INTO `mahasiswa` VALUES (1, 1234567890, 'Ali', 3, '2020-10-13 09:28:42', NULL);
+INSERT INTO `mahasiswa` VALUES (2, 1234567891, 'Panji', 3, '2020-10-13 12:19:46', '2020-10-13 15:04:06');
+INSERT INTO `mahasiswa` VALUES (3, 1234567892, 'Astrid', 1, '2020-10-13 12:20:05', NULL);
+INSERT INTO `mahasiswa` VALUES (4, 1234567893, 'Alief', NULL, '0000-00-00 00:00:00', NULL);
+INSERT INTO `mahasiswa` VALUES (5, 1234567894, 'Fariz', 3, '0000-00-00 00:00:00', NULL);
+INSERT INTO `mahasiswa` VALUES (6, 1234567895, 'Jojo', 5, '0000-00-00 00:00:00', NULL);
+INSERT INTO `mahasiswa` VALUES (7, 1234567896, 'Jeje update', 2, '2020-10-16 15:44:39', '2020-10-19 11:46:30');
+INSERT INTO `mahasiswa` VALUES (9, 1234567898, 'Nico', 5, '2020-10-16 15:46:24', '2020-10-16 15:46:24');
+INSERT INTO `mahasiswa` VALUES (10, 1234567899, 'tiara', 4, '2020-10-19 10:51:28', NULL);
+INSERT INTO `mahasiswa` VALUES (11, 1234567900, 'Vale', 3, '2020-10-25 16:44:04', NULL);
+
+-- ----------------------------
+-- Table structure for mata_kuliah
+-- ----------------------------
+DROP TABLE IF EXISTS `mata_kuliah`;
+CREATE TABLE `mata_kuliah`  (
+  `id_mata_kuliah` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_mata_kuliah`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for ruang
+-- ----------------------------
+DROP TABLE IF EXISTS `ruang`;
+CREATE TABLE `ruang`  (
+  `id_ruang` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_ruang`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `default_password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_user`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, 'aliansyah', 'ALI', '$2a$10$I3OnaJg9N.crtI5Zu8qlDe.aI/PhMLdMV8PClfgiYPBXZQvzD5yRO', '123');
+
+SET FOREIGN_KEY_CHECKS = 1;
